081b27241909e226d2a224644ef1bc43e9747ce0
jjungo <j.jungo@gmail.com>
2016-02-26 14:07:33 +0100

    update licence
    remove tmp file

--- lib/solhsm_network.c
+++ lib/solhsm_network.c
@@ -1,10 +1,9 @@
-/* <@LICENSE>
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to you under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at:
+/*
+ * Copyright 2016 Joël Jungo, Titouan Mesot
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
  *     http://www.apache.org/licenses/LICENSE-2.0
  * 
@@ -13,8 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * </@LICENSE>
+ *
  */
+
 /**
  * This small api provide a simple way to send and receive data,
  * on the HSM through zmq context see solhsm_network.h for details.
